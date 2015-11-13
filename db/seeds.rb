cbeef2be592e688ce1829e18034be9c3b10513b9
Arcangelo Petretta <arcpetretta@gmail.com>
2015-11-13 15:39:31 -0500

    Creates Seed file.
    Creates Seed file which contains 4 decks (Retro Video Game Trivia, Marvel Trivia, Movie Trivia and Basketball Trivia). Also creates 5 cards for each deck.

---
+++ db/seeds.rb
@@ -0,0 +1,33 @@
+Deck.create([{category: 'Video Games', name: 'Retro Game Trivia'},
+             {category: 'Comics' , name: 'Marvel Trivia' },
+             {category: 'Pop Culture' , name: 'Movie Trivia' },
+             {category: 'Sports', name:'Basketball Trivia' }])
+
+
+#Retro Game Trivia
+Card.create([{deck_id: 1, question:'What highly addictive puzzle game designed by a Russian programmer features Russian buildings, dancers and music?', answer:'Tetris',difficulty: 1},
+             {deck_id: 1, question:'Before becoming a plumber in the 1983 game Mario Bros., Mario had a different profession. What was it? ', answer:'Pizza Maker',difficulty: 1},
+             {deck_id: 1, question:'Who is Bagman? ', answer:'Retired safe cracker',difficulty: 1},
+             {deck_id: 1, question:'Who says "@!#?@!" every time he is hit by something? ', answer:'Donkey Kong',difficulty: 1},
+             {deck_id: 1, question:'What must be destroyed in Star Wars to get to the next level of difficulty? ', answer:'Death Star',difficulty: 1},
+
+#Marvel Trivia Cards
+             {deck_id: 2, question:'What is Peter Parker\'s middle name?', answer:'Benjamin',difficulty: 1},
+             {deck_id: 2, question:'Which X-Men villain possessed the Infinity Gems? ', answer:'Thanos',difficulty: 1},
+             {deck_id: 2, question:'Which comic book writer created Hawkeye?', answer:'Stan Lee',difficulty: 1},
+             {deck_id: 2, question:'Who killed Charles Xavier in "Avengers vs. X-Men"?', answer:'Cyclops',difficulty: 1},
+             {deck_id: 2, question:'Which member of the Guardians of the Galaxy is related to Thanos?', answer:'Gamora',difficulty: 1},
+
+#Movie Trivia Cards
+             {deck_id: 3, question:'Who created the musical score for "The Good, The Bad and The Ugly"', answer:'Ennio Morricone',difficulty: 1},
+             {deck_id: 3, question:'What movie was the first the first color film? ', answer:'The Wizard of Oz',difficulty: 1},
+             {deck_id: 3, question:'After Top Gun was shown in cinemas, recruitment into the Navy by young men went up by how many percent?', answer:'500',difficulty: 1},
+             {deck_id: 3, question:'Who was the first character to fart in a Disney Movie', answer:'Pumbaa',difficulty: 1},
+             {deck_id: 3, question:'Peter Ostrum, Charlie from Willy Wonka and the Chocolate Factory, never acted in another movie after that and is now working as a?...', answer:'Veterinarian',difficulty: 1},
+
+#Basketball Trivia Cards
+             {deck_id: 4, question:'What team won the very first NBA game?', answer:'New York Knicks',difficulty: 1},
+             {deck_id: 4, question:'What NBA player scored 100 points on March 2, 1962?', answer:'Wilt Chamberlain',difficulty: 1},
+             {deck_id: 4, question:'What player won All-Star Game MVP, NBA MVP, and NBA Finals MVP awards in 2000?', answer:'Shaq',difficulty: 1},
+             {deck_id: 4, question:'Who was the first Chinese player to play in an NBA game?', answer:'Wang Zhizhi',difficulty: 1},
+             {deck_id: 4, question:'On Dec. 9, 2004 what NBA player scored 13 points in 33 seconds to seal a win against the San Antonio Spurs', answer:'Tracy McGrady',difficulty: 1}])

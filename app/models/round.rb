7337744da0f932d3b19a39a8b0f6f83a9fba4e77
Salim Rahimi <email": null>
2015-11-13 19:17:39 -0500

    Get game to pass variables between get and put

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,33 @@
 class Round < ActiveRecord::Base
   belongs_to :user
   belongs_to :deck
+
+  @@points_for_card = {}, @@cards = [], @@card = nil
+
+  def self.points_for_card=(value)
+    @@points_for_card = value
+  end
+
+  def self.cards=(value)
+    @@cards = value
+  end
+
+  def self.card=(value)
+    @@card = value
+  end
+
+  def self.points_for_card
+    @@points_for_card
+  end
+
+  def self.cards
+    @@cards
+  end
+
+  def self.card
+    @@card
+  end
+
+
+
 end

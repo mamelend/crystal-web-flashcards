4af49885014840ac595e504796a60f91f8006872
Salim Rahimi <email": null>
2015-11-15 19:29:39 -0500

    check guess correctness

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -7,6 +7,7 @@ get '/rounds/:id' do
       @cards = @deck.cards.shuffle
       @cards.each { @points += "2" }
       @card = @cards.pop
+      @is_correct = "2"
     else
       "Game not started"
     end
@@ -15,40 +16,45 @@ get '/rounds/:id' do
 end
 
 put '/rounds/:id' do
-  @round = Round.find(params[:id])
-  @round.total_guesses += 1
-  @deck = Deck.find(@round.deck_id)
-  @cards = @deck.cards
-  @index_separator = @cards.first.id - 1
-  @card = Card.find(params[:card_id])
-  @points = params[:points]
-  if params[:guess] == @card.answer
-    @round.points += @points[@card.id - @index_separator].to_i * @card.difficulty
-    @points[@card.id - @index_separator].to_i  == 2 ? @round.correct += 1 : nil
-    @points[@card.id - @index_separator] = "3"
-    @is_correct = 1
-    erb :'/rounds/result'
-  else
-    @points[@card.id - @index_separator].to_i > 0 ? @points[@card.id - @index_separator] = (@points[@card.id - @index_separator].to_i - 1).to_s : nil
-    @is_correct = 0
-    erb :'/rounds/result'
+  @is_correct = params[:is_correct]
+  if @is_correct == "2"
+    @round = Round.find(params[:id])
+    @round.total_guesses += 1
+    @deck = Deck.find(@round.deck_id)
+    @cards = @deck.cards
+    @index_separator = @cards.first.id - 1
+    @card = Card.find(params[:card_id])
+    @points = params[:points]
+    if params[:guess] == @card.answer
+      @round.points += @points[@card.id - @index_separator].to_i * @card.difficulty
+      @points[@card.id - @index_separator].to_i  == 2 ? @round.correct += 1 : nil
+      @points[@card.id - @index_separator] = "3"
+      @is_correct = "1"
+    else
+      @points[@card.id - @index_separator].to_i > 0 ? @points[@card.id - @index_separator] = (@points[@card.id - @index_separator].to_i - 1).to_s : nil
+      @is_correct = "0"
+    end
+
+    @round.save
+
   end
 
-  @round.save
-
-  @cards = @deck.cards.reject { |card| @points[card.id - @index_separator] == "3" }
-
-  if @cards.count > 0
-    @cards = @cards.shuffle
-    @card = @cards.pop
-    erb :'rounds/round'
-  else
-    erb :'/rounds/over'
-  end
+    if @is_correct == "3"
+      @round = Round.find(params[:id])
+      @deck = Deck.find(@round.deck_id)
+      @cards = @deck.cards
+      @index_separator = @cards.first.id - 1
+      @points = params[:points]
+      @cards = @deck.cards.reject { |card| @points[card.id - @index_separator] == "3" }
+      @is_correct = "2"
+      if @cards.count > 0
+        @cards = @cards.shuffle
+        @card = @cards.pop
+        erb :'/rounds/round'
+      else
+        erb :'/rounds/over'
+      end
+    elsif @is_correct == "1" || @is_correct == "0"
+      erb :'/rounds/round'
+    end
 end
-
-get '/rounds/:id/continue' do
-  erb :'rounds/round'
-end
-
-
